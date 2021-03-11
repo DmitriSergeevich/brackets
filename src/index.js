5598fc064cdbe5a8506a69aa696e9d9ac0715bf4
Dmitry Likhachev <valomon@yandex.ru>
2021-03-11 15:00:51 +0500

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, bracketsConfig) {
+  let combination = [];
+  for(let i=0; i<bracketsConfig.length; i++){
+  	combination.push(bracketsConfig[i].join(''))
+  }
+  let count =0;  
+  while (count < combination.length) {
+    if (s.includes(combination[count])) {
+      s = s.replace(combination[count], '')
+      count = 0;
+    }else count++;
+  } return s.length === 0 ? true: false;
 }
+
